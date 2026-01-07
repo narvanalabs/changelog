f0c2f5cb76bb1c6439158a31bd6843aee6ddd03d
Daniel Togey <dannykosgei@gmail.com>
2026-01-07 21:33:51 +0300

    chore(consts): Update site metadata and add website URL constant
    - Update SiteTitle from "Starlog" to "Narvana"
    - Update SiteDescription to reflect Narvana platform release notes and changelog
    - Add MainWebsiteUrl constant pointing to https://narvana.io
    - Align global constants with rebranded platform identity

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SiteTitle = "Starlog";
-export const SiteDescription = "Welcome to my website!";
+export const SiteTitle = "Narvana";
+export const SiteDescription = "Release notes and changelog for Narvana - the modern PaaS platform";
+export const MainWebsiteUrl = "https://narvana.io";
